b19f067ed20b9ebb894cd0e5334c018e0272185b
wang <654344155@qq.com>
2020-12-16 15:46:43 +0900

    euphoim

--- geneDensity/makeDensity.sh
+++ geneDensity/makeDensity.sh
@@ -1,10 +1,11 @@
-touch genometable.split
 res=$1
 
-for i in `seq 1 22` X
+touch genometable.split
+
+for i in `seq 1 19` X
 do
 	chromosome=chr$i
-	length=`awk '$1 == "'$chromosome'" {print $2}' genome_table`
+	length=`awk '$1 == "'$chromosome'" {print $2}' mm10_genome_table`
 	endlength=`expr $length + $res`
 	
 	seq 0 $res $length > start
@@ -12,7 +13,7 @@ do
 	paste start end | sed "s/^/${chromosome}\t/g"  >> genometable.split
 done
 
-bedtools coverage -a genometable.split -b refFlat.bed |cut -f 1-4 > geneDensity$res.txt
+bedtools coverage -a genometable.split -b mm10_refFlat.bed |cut -f 1-4 > mm10_geneDensity$res.txt
 
 
 rm start end genometable.split
