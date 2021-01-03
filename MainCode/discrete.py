de39254a457795024b54275000f22564da546ee4
2019mac <wangjk321@gmail.com>
2021-01-03 17:56:31 +0900

    mac2019

--- MainCode/discrete.py
+++ MainCode/discrete.py
@@ -1,1 +1,7 @@
-v
+from MultiTypeScore import *
+
+def getDiscrete(path,res,chr,mode,parameter):
+    ob = multiScore(path,res,chr)
+    score = ob.obtainOneScore(mode,parameter)
+    return(score)
+    
