9a333f14d3370af0c72cf0a80ee12440fb49d39b
2019mac <wangjk321@gmail.com>
2021-01-04 12:07:06 +0900

    mac2019

--- MainCode/discrete.py
+++ MainCode/discrete.py
@@ -1,14 +1,15 @@
 from MultiTypeScore import *
+from calculateMetrics import *
 
-def getDiscrete(path,res,chr,mode,parameter):
+def getDiscrete(path,res,chr,mode,parameter,control_path=""):
     ob = multiScore(path,res,chr)
     score = ob.obtainOneScore(mode,parameter)
     state = np.zeros(score.shape[0])*np.NaN
     if mode == "PC1":
         state[score.iloc[:,3] > 0] = 1
         state[score.iloc[:,3] < 0] = -1
-    elif mode == "tad":
-        pass
+    elif mode == "border":
+        tad = TADcallIS()
 
     score.iloc[:,3] =state
 
@@ -41,12 +42,12 @@ class multiSampleDiscrete:
         ebin = end//self.res
 
         from callDirectionalTAD import PlotTAD
-        plt.figure(figsize=(10,9+self.nScore*1))
-        plt.subplot2grid((5+self.nScore,11),(0,0),rowspan=5,colspan=10)
+        plt.figure(figsize=(10,9+self.nScore))
+        plt.subplot2grid((5+int(self.nScore/1.5),11),(0,0),rowspan=5,colspan=10)
         hp = PlotTAD(hic_path,self.res,start,end,clmax=clmax)
         hp.draw()
 
-        plt.subplot2grid((5+self.nScore,11),(6,0),rowspan=self.nScore//3,colspan=11)
+        plt.subplot2grid((5+int(self.nScore/1.5),11),(5,0),rowspan=(self.nScore//4)+1,colspan=11)
         df = self.getMultiDiscrete().iloc[sbin:ebin,:].T
         plt.imshow(df,aspect="auto",interpolation=interpolation,vmin=heatmin)
         plt.yticks(range(self.nScore),self.namelist)
