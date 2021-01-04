c22c181cc45744e09a7524f1e33fb1b1fbfdf3f0
2019mac <wangjk321@gmail.com>
2021-01-04 10:40:58 +0900

    mac2019

--- MainCode/discrete.py
+++ MainCode/discrete.py
@@ -10,15 +10,38 @@ def getDiscrete(path,res,chr,mode,parameter):
 
     return(score)
 
-def getMultiDiscrete(sampleList, labels, res, chr, mode, UniqueParameter):
-    for i,path in enumerate(sampleList):
-        if i==0: metricMT = getDiscrete(path,res,chr,mode,UniqueParameter)
-        else:
-            next = getDiscrete(path,res,chr,mode,UniqueParameter).iloc[:,3:4]
-            metricMT = pd.concat([metricMT,next],axis=1)
+class multiSampleDiscrete:
+    def __init__(self,pathlist,namelist,res,chr,mode,UniqueParameter):
+        self.pathlist = pathlist
+        self.namelist = namelist
+        self.res = res
+        self.chr = chr
+        self.mode = mode
+        self.UniqueParameter = UniqueParameter
+        self.nScore = len(namelist)
 
-    if mode != "raw":
+    def getMultiDiscrete(self):
+        for i,path in enumerate(self.pathlist):
+            if i==0: metricMT = getDiscrete(path,self.res,self.chr,self.mode,self.UniqueParameter)
+            else:
+                next = getDiscrete(path,self.res,self.chr,self.mode,self.UniqueParameter).iloc[:,3:4]
+                metricMT = pd.concat([metricMT,next],axis=1)
+
         metricMT.index = metricMT.start.tolist()
         metricMT = metricMT.iloc[:,3:]
-    metricMT.columns = labels
-    return(metricMT)
+        metricMT.columns = self.namelist
+        return(metricMT)
+
+def plot_discrete(mt,res,hic_path,start,end,clmax=100,heatmin=None):
+    sbin = start//self.res
+    ebin = end//self.res
+
+    from callDirectionalTAD import PlotTAD
+    plt.figure(figsize=(10,9+self.nScore*1))
+    plt.subplot2grid((5+self.nScore,11),(0,0),rowspan=5,colspan=10)
+    hp = PlotTAD(hic_path,self.res,start,end,clmax=clmax)
+    hp.draw()
+
+    plt.subplot2grid((5+self.nScore,11),(5,0),rowspan=self.nScore//2,colspan=11)
+    df = self.score.iloc[sbin:ebin,:].T
+    plt.imshow(df,aspect="auto",interpolation='none',cmap="Purples",vmin=heatmin)
