b024710c3cccbb0e846ccd5d0abc96c2e5067a2e
2019mac <wangjk321@gmail.com>
2021-01-05 12:01:09 +0900

    2019mac

--- MainCode/discrete.py
+++ MainCode/discrete.py
@@ -1,11 +1,12 @@
 from MultiTypeScore import *
 from calculateMetrics import *
+import os
 
 def getDiscrete(path,res,chr,mode,parameter,control_path=""):
     if mode == "PC1":  #Acompartment~1,Bcompartment~-1
         ob = multiScore(path,res,chr)
         score = ob.obtainOneScore(mode,parameter)
-        state = np.array(["none"]*score.shape[0],dtype=object)
+        state = np.array(["NA"]*score.shape[0],dtype=object)
         state[score.iloc[:,3] > 0] = "CompartA"
         state[score.iloc[:,3] < 0] = "CompartB"
         score.iloc[:,3] =state
@@ -16,7 +17,7 @@ def getDiscrete(path,res,chr,mode,parameter,control_path=""):
         # CIC: postive 1 stronger boundary. negative -1 weaker boundart
         ob = multiScore(path,res,chr,control_path=control_path)
         score = ob.obtainTwoScore(mode,parameter)
-        state = np.array(["none"]*score.shape[0],dtype=object)
+        state = np.array(["NA"]*score.shape[0],dtype=object)
         state[score.iloc[:,3] > 0] = "up"+mode
         state[score.iloc[:,3] < 0] = "down"+mode
         score.iloc[:,3] =state
@@ -25,7 +26,7 @@ def getDiscrete(path,res,chr,mode,parameter,control_path=""):
         ob = multiScore(path,res,chr,control_path=control_path)
         score = ob.obtainTwoScore(mode,parameter)
         thresh = score.CorrD.describe()[5]
-        state = np.array(["none"]*score.shape[0],dtype=object)
+        state = np.array(["NA"]*score.shape[0],dtype=object)
         state[score.iloc[:,3] >= thresh] = str("highCorr")
         state[score.iloc[:,3] < thresh] = str("lowCorr")
         score.iloc[:,3] =state
@@ -82,6 +83,40 @@ class multiTypeDiscrete:
     def makecsv(self,outname="multiDiscrete.txt"):
         self.multiDiscrete().iloc[:,3:].to_csv(outname,sep="\t",index=None)
 
+    def useHMM(self):
+        self.makecsv()
+        import rpy2.robjects as robjects
+        r = robjects.r
+        if not os.path.exists(path):
+            os.mkdir("seqHMM")
+
+        r("library(seqHMM)")
+        r("mt <- read.csv('multiDiscrete.txt',sep='\t')")
+        r("mt[mt=='NA'] <- NA")
+
+        r("mtlist <- list()")
+        r("for(i in seq(1,dim(mt)[2])){mtlist[[i]] <- seqdef(t(as.matrix(mt[,i])))}")
+
+        r("pdf('seqHMM/observedstate.pdf',width=10,height = 8)")
+        r("ssplot(mtlist, title = 'Observed states plots')")
+        r("dev.off()")
+
+        r("mod <- build_hmm(observations = mtlist, n_states = 5)")
+        r("hmm <- fit_model(mod,global_step = TRUE, local_step = TRUE,threads=10,control_em = list(restart=list(times = 100)))")
+
+        r("tMT <- hmm$model$transition_probs")
+        r("write.table(tMT,'seqHMM/transitionMatrix.txt',sep='\t')")
+
+        r("hMT <- hidden_paths(hmm$model)")
+        r("write.table(hMT,'seqHMM/hiddenState.txt',sep='\t')")
+
+        r("emp<- hmm$model$emission_probs")
+        r("eMT <- matrix(data = NA,dim(emp[[1]])[1],length(emp))")
+        r("label <- c()")
+        r("for(i in seq(1,length(emp))){eMT[,i] <- emp[[i]][,1];label[i] <- colnames(emp[[i]])[1]}")
+        r("colnames(eMT) <- label")
+        r("row.names(eMT) <- row.names(emp[[1]])")
+        r("write.table(hMT,'seqHMM/emissionMatrix.txt',sep='\t')")
 
 class multiSampleDiscrete:
     def __init__(self,pathlist,namelist,res,chr,mode,UniqueParameter):
@@ -119,17 +154,3 @@ class multiSampleDiscrete:
         df = self.getMultiDiscrete().iloc[sbin:ebin,:].T
         plt.imshow(df,aspect="auto",interpolation=interpolation,vmin=heatmin)
         plt.yticks(range(self.nScore),self.namelist)
-
-def plot_discrete(mt,res,hic_path,start,end,clmax=100,heatmin=None):
-    sbin = start//self.res
-    ebin = end//self.res
-
-    from callDirectionalTAD import PlotTAD
-    plt.figure(figsize=(10,9+self.nScore*1))
-    plt.subplot2grid((5+self.nScore,11),(0,0),rowspan=5,colspan=10)
-    hp = PlotTAD(hic_path,self.res,start,end,clmax=clmax)
-    hp.draw()
-
-    plt.subplot2grid((5+self.nScore,11),(5,0),rowspan=self.nScore//2,colspan=11)
-    df = self.score.iloc[sbin:ebin,:].T
-    plt.imshow(df,aspect="auto",interpolation='none',cmap="Purples",vmin=heatmin)
