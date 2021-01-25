128443e3da5c3a91ca46ae792d538429956e9587
2019mac <wangjk321@gmail.com>
2021-01-25 19:01:33 +0900

    029

--- MainCode/discrete.py
+++ MainCode/discrete.py
@@ -198,4 +198,4 @@ class multiSampleDiscrete:
         df = self.getMultiDiscrete(label=False).iloc[sbin:ebin,:].T
         plt.imshow(df,aspect="auto",interpolation=interpolation,vmin=heatmin)
         plt.yticks(range(self.nScore),self.namelist)
-        plt.xticks(hp.ticks_pos,hp.mark)
+        plt.xticks(np.arange(0,df.shape[1]+1,(df.shape[1])/5),hp.mark)
