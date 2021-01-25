6516f6bdf05328892a828f390ead00ab5e172879
2019mac <wangjk321@gmail.com>
2021-01-25 19:06:14 +0900

    029

--- MainCode/Multi_samples_metrics.py
+++ MainCode/Multi_samples_metrics.py
@@ -115,7 +115,7 @@ class repQC:
         df = self.score.iloc[sbin:ebin,:].T
         plt.imshow(df,aspect="auto",interpolation='none',cmap="Purples",vmin=heatmin)
         plt.yticks(range(len(self.namelist)),self.namelist)
-        plt.xticks(hp.ticks_pos,hp.mark)
+        plt.xticks(np.arange(0,df.shape[1]+1,(df.shape[1])/5),hp.mark)
 
     def anova_like(self,start,end):
         sbin = start//self.res
