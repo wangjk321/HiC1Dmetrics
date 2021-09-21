a3e4a8fb7e4d615706397056bcee6378f8bf21b1
2019mac <wangjk321@gmail.com>
2021-09-21 11:36:30 +0900

    support cooler

--- h1d/extract/coolerdump.sh
+++ h1d/extract/coolerdump.sh
@@ -10,6 +10,8 @@ pwd=$(cd $(dirname $0) && pwd)
 
 cooler dump -r $chr --join $cool | cut -f 2,5,7  > sparse.temp
 
+mkdir -p $outname/${binsize}
+ 
 outdir=$outname/${binsize}/${chr}.matrix.gz
 
 $pwd/convert_JuicerDump_to_dense.py sparse.temp $outdir $gt $chr $binsize
