e224c0c9343e2d59d29b8c5e8cbb2c9056e917f4
2019mac <wangjk321@gmail.com>
2021-10-29 17:19:07 +0900

    error information

--- h1d/extract/coolerdump.sh
+++ h1d/extract/coolerdump.sh
@@ -6,15 +6,17 @@ chr=$3
 gt=$4
 outname=$5
 
-chrlen=$(cooler dump -t chroms $cool |head -1 |cut -f 1 |awk '{print length}')
 
 pwd=$(cd $(dirname $0) && pwd)
 chrnum=$(echo $chr |sed 's/chr//g')
+chrlen=$(cooler dump -t chroms $cool |head -1 |cut -f 1 |awk '{print length}')
 
 if [ $chrlen > 3 ]
 then
+  echo 'chang'
   cooler dump -r $chr --join $cool | cut -f 2,5,7  > sparse.temp
 else
+  echo 'duan'
   cooler dump -r $chrnum --join $cool | cut -f 2,5,7  > sparse.temp
 fi
 
