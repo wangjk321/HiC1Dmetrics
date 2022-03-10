f5a1a1f544f41c2024c845efdeefdb7bfc436f56
2019mac <wangjk321@gmail.com>
2022-03-10 10:41:21 +0900

    v0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='h1d',
-    version='0.2.0',
+    version='0.2.1',
     author='wangjiankng',
     author_email='wangjk321@gmail.com',
     url='https://github.com/wangjk321/HiC1Dmetrics',
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     install_requires=["pandas","numpy","scikit-learn","scipy",
 		"matplotlib","seaborn","multiprocess",
-		"fithic==2.0.7","statsmodels",'cooler==0.8.2'],
+		"fithic==2.0.7","statsmodels",'cooler==0.8.*'],
     classifiers=[
         "Programming Language :: Python :: 3",
 	"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
